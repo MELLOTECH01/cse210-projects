938bc1194f928a5c8de5197c94f3915014163bdc
mellotech01 <dayooluwasuyi@gmail.com>
2024-05-05 17:01:37 -0700

    prep 1 to prep 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("");
+        Console.Write("What is your grage percentage in CSE210? ");
+        string gradePercentage = Console.ReadLine();
+        int gradePercentageConvert = int.Parse(gradePercentage);
+        string letter;
+        int remainder = gradePercentageConvert % 10;
+        string sign;
+
+        if (gradePercentageConvert >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentageConvert >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentageConvert >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentageConvert >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (remainder >= 7 && !(letter == "A" || letter == "F"))
+        {
+            sign = "+";
+        }
+        else if (remainder < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your grade is {letter}{sign}");
+        if (gradePercentageConvert >= 70)
+        {
+            Console.Write("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.Write("We are sorry! We enconrage you to try again and do better.");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int magicNumberConvert = int.Parse(magicNumber);
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int guessConvert;
+        int noOfGuesses = 0;
+        string response;
+
+        do
+        {
+            do
+            {
+                Console.Write("What is your guess? ");
+                string guess = Console.ReadLine();
+                guessConvert = int.Parse(guess);
+                noOfGuesses++;
+
+                if (guessConvert > magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessConvert < magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (guessConvert != magicNumber);
+            Console.WriteLine($"You made {noOfGuesses} guesses!");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.Write("Do you still want to play again? ");
+            response = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("");
+        } while (response == "yes");
     }
 }
