2e0acd27628656709f18f4bf5c5fa11041f0072c
mellotech01 <dayooluwasuyi@gmail.com>
2024-05-12 17:10:03 -0700

    write codes as instructed in learning02/Resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job
+        {
+            _company = "Microsoft",
+            _jobTitle = "Software Engineer",
+            _startYear = 2019,
+            _endYear = 2022
+        };
+
+        Job job2 = new Job
+        {
+            _company = "Apple",
+            _jobTitle = "Manager",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        // job1.DisplayJobDetails();
+        // job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "Dayo Oluwasuyi";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
