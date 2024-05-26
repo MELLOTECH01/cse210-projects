142c6a7af6610a634424b192ccb9343821fa5b61
mellotech01 <dayooluwasuyi@gmail.com>
2024-05-25 20:02:03 -0700

    Develop03 - Scripture Memorizer Program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,174 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// My Quick Web Search:
+// https://stackoverflow.com
+// https://www.geeksforgeeks.org
+
+// Added a library of scriptures and program choose at random to present to the user.
+
+class Reference
+{
+    // Use nullable int data type
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int? _endVerse;
+
+    // Constructor for single verse reference
+    public Reference(string book, int chapter, int verse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = null;
+    }
+
+    // Constructor for verse range reference
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
+    }
+
+    public string GetDisplayText()
+    {
+        return _endVerse.HasValue
+            ? $"{_book} {_chapter}:{_verse}-{_endVerse.Value}"
+            : $"{_book} {_chapter}:{_verse}";
+    }
+}
+
+class Word
+{
+    private string _text;
+    private bool _isHidden;
+
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public void Show()
+    {
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public string GetDisplayText()
+    {
+        return _isHidden ? "____" : _text;
+    }
+}
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(w => new Word(w)).ToList();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        for (int i = 0; i < numberToHide; i++)
+        {
+            var visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+            if (visibleWords.Count == 0) return;
+
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_reference.GetDisplayText()}\n{string.Join(" ", _words.Select(w => w.GetDisplayText()))}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
+
+class ScriptureLibrary
+{
+    private List<Scripture> _scriptures;
+    private Random _random;
+
+    public ScriptureLibrary()
+    {
+        _scriptures = new List<Scripture>();
+        _random = new Random();
+    }
+
+    public void LoadDefaultScriptures()
+    {
+        // Add some default scriptures to the library
+        _scriptures.Add(new Scripture(new Reference("John", 3, 16), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."));
+        _scriptures.Add(new Scripture(new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight."));
+        _scriptures.Add(new Scripture(new Reference("Psalm", 23, 1), "The Lord is my shepherd, I lack nothing."));
+        _scriptures.Add(new Scripture(new Reference("Philippians", 4, 13), "I can do all this through him who gives me strength."));
+    }
+
+    public Scripture GetRandomScripture()
+    {
+        int index = _random.Next(_scriptures.Count);
+        return _scriptures[index];
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        ScriptureLibrary library = new ScriptureLibrary();
+        library.LoadDefaultScriptures();
+
+        while (true)
+        {
+            Scripture scripture = library.GetRandomScripture();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(scripture.GetDisplayText());
+                Console.WriteLine("\nPress Enter to hide words or type 'quit' to exit.");
+
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                {
+                    return;
+                }
+
+                scripture.HideRandomWords(3);
+
+                if (scripture.IsCompletelyHidden())
+                {
+                    Console.Clear();
+                    Console.WriteLine("All words are hidden. You have memorized the scripture reference!");
+                    break;
+                }
+            }
+        }
+
     }
 }
