688ea9aebc00710b775226d891c13bb9ada65956
mellotech01 <dayooluwasuyi@gmail.com>
2024-05-30 22:53:45 -0700

    InheritancePrepare

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Console.WriteLine("Hello Learning04 World!");
+
+        // Creating a Math assignment
+		MathAssignment mathAssignment = new MathAssignment("Dayo Oluwasuyi", "Fractions", "7.3", "3-10, 20-21");
+		Console.WriteLine(mathAssignment.GetSummary());
+		Console.WriteLine(mathAssignment.GetHomeworkList());
+
+		// Creating a Writing assignment
+		WritingAssignment writingAssignment = new WritingAssignment("Olaitan Omoniyi", "European History", "The Causes of World War II");
+		Console.WriteLine(writingAssignment.GetSummary());
+		Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class WritingAssignment: Assignment
+{
+	private string _title;
+
+	public WritingAssignment(string studentName, string topic, string title)
+		: base(studentName, topic)
+	{
+		_title = title;
+	}
+
+	public string GetWritingInformation()
+	{
+		return $"{_title} by {_studentName}";
+	}
+}
