a3ed5c2904f3537f54b8be1126b26c6e75478ef5
mellotech01 <dayooluwasuyi@gmail.com>
2024-06-05 17:49:56 -0700

    POlymorphism with Exercise Tracking

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,172 @@
 using System;
+using System.Collections.Generic;
+
+
+// My Quick Web Search:
+// https://stackoverflow.com
+// https://www.geeksforgeeks.org
+// w3schools c#
+// MDN
+// learn.microsoft.com
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        // Console.WriteLine("Hello Foundation4 World!");
+
+        // Create activities
+        Activity runningActivity = new Running(new DateTime(2022, 11, 3), 30, 4.8);
+        Activity cyclingActivity = new Cycling(new DateTime(2022, 11, 3), 45, 20.0);
+        Activity swimmingActivity = new Swimming(new DateTime(2022, 11, 3), 60, 40);
+
+        // Add activities to a list
+        List<Activity> activities = new List<Activity> { runningActivity, cyclingActivity, swimmingActivity };
+
+        // Display summaries for each activity
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+
+    }
+}
+
+// Base class for Activity
+class Activity
+{
+    private DateTime _date;
+    private int _lengthInMinutes;
+
+    public Activity(DateTime date, int lengthInMinutes)
+    {
+        _date = date;
+        _lengthInMinutes = lengthInMinutes;
+    }
+
+    public DateTime GetDate()
+    {
+        return _date;
+    }
+
+    public int GetLengthInMinutes()
+    {
+        return _lengthInMinutes;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0.0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0.0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0.0;
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{_date.ToString("dd MMM yyyy")} Activity ({_lengthInMinutes} min) - Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km";
+    }
+}
+
+// Derived class for Running
+class Running : Activity
+{
+    private double _distance;
+
+    public Running(DateTime date, int lengthInMinutes, double distance)
+        : base(date, lengthInMinutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return _distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetLengthInMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetLengthInMinutes() / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{GetDate().ToString("dd MMM yyyy")} Running ({GetLengthInMinutes()} min) - Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km";
+    }
+}
+
+// Derived class for Cycling
+class Cycling : Activity
+{
+    private double _speed;
+
+    public Cycling(DateTime date, int lengthInMinutes, double speed)
+        : base(date, lengthInMinutes)
+    {
+        _speed = speed;
+    }
+
+    public override double GetDistance()
+    {
+        return (_speed * GetLengthInMinutes()) / 60;
+    }
+
+    public override double GetSpeed()
+    {
+        return _speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / _speed;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{GetDate().ToString("dd MMM yyyy")} Cycling ({GetLengthInMinutes()} min) - Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km";
+    }
+}
+
+// Derived class for Swimming
+class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int lengthInMinutes, int laps)
+        : base(date, lengthInMinutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return (_laps * 50) / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / GetLengthInMinutes()) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return GetLengthInMinutes() / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{GetDate().ToString("dd MMM yyyy")} Swimming ({GetLengthInMinutes()} min) - Distance: {GetDistance():0.0} km, Speed: {GetSpeed():0.0} kph, Pace: {GetPace():0.0} min per km";
     }
 }
